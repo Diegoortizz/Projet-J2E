07ac682f719b74eea206f9a5ccb730e470f1ba6a
crypi <crypi@192.168.1.15>
2018-11-21 16:08:00 +0100

    Création du DAO, du DAOException et du DataSourceFactory.

---
+++ ProjetJEE/src/main/Source Packages/Modele/DAO.java
@@ -0,0 +1,15 @@
+
+import javax.sql.DataSource;
+
+
+public class DAO {
+    
+    private final DataSource myDataSource;
+    
+    public DAO(DataSource dataSource) {
+		this.myDataSource = dataSource;
+    }
+    
+    //Méthodes
+    
+}
